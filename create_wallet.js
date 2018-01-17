b8ad574e13ff7c0aeeee2d54e439ff76c15f17b1
Noah Hunter <nhhntr11@gmail.com>
2018-01-17 00:48:41 +0000

    Adding multi address wallet tutorial

--- create_wallet.js
+++ create_wallet.js
@@ -1,14 +1,25 @@
 var ducatuscore = require('ducatuscore-lib');
 var fs = require('fs');
 
-// Create a new address
-var privateKey = new ducatuscore.PrivateKey();
-var address = privateKey.toAddress();
+// Create a new HD wallet
+var hdPrivateKey = new ducatuscore.HDPrivateKey();
 
-console.log("Wallet Address: " + address.toString());
+// This wallet can derive multiple addresses:
+var derived = [];
+// Let's generate a hundred new addresses
+for (i = 0; i < 100; i++) {
+	derived.push(hdPrivateKey.derive(0).derive(i));
+}
 
-// Output to Wallet Import Format (WIF)
-console.log("WIF: " + privateKey.toWIF());
+// Now print out the public keys/addresses
+for (i = 0; i < 100; i++) {
+	console.log(derived[i].privateKey.toAddress().toString());
+}
 
-// Save the wallet to disk
-fs.writeFileSync('wallet.wif', privateKey.toWIF());
+// Output to Wallet Import Format of a wallet (in this case, let's use the first wallet created)
+console.log("WIF: " + derived[0].privateKey.toWIF());
+
+console.log("Wallet Address: " + derived[0].privateKey.toAddress().toString());
+
+// Save the first derived wallet to disk
+fs.writeFileSync('wallet.wif', derived[0].privateKey.toWIF());
